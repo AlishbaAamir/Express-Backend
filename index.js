0021c1c2f87f39476b782fe5c1a95a434f5be868
Alishba Aamir <amiralishba040@gmail.com>
2023-08-27 14:02:26 +0500

    Backend

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.SERVER_PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+
+ app.use('/api', require('./api/User/Router'))
+ app.use('/api', require('./api/Category/Router'))
+ app.use('/api', require('./api/Restuarent/Router'))
+ app.use('/api', require('./api/Items/Router'))
+
+ 
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}` )
+})
